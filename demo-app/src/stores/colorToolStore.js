8ee66ea39c2916e64ebce97ab6355b48f288c118
jelias <jacob_elias@intuit.com>
2021-06-29 14:10:53 -0700

    following along

--- demo-app/src/stores/colorToolStore.js
+++ demo-app/src/stores/colorToolStore.js
@@ -1,4 +1,6 @@
 import { createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
 
 import { colorToolReducer } from '../reducers/color-tool';
 
